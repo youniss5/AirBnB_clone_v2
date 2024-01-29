db20c94653e5b2f24f43538e04503abeab558bf7
youniss5 <ahmedhyouniss@gmail.com>
2024-01-28 19:24:46 -0800

    hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application"""
+
+from flask import Flask
+
+appl = Flask(__name__)
+
+#route for URL
+@appl.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ print hello hbnb!"""
+    return "Hello HBNB!"
+
+#route for '/hbnb'
+@appl.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ print hbnb"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    #listen to all available network
+    appl.run(host='0.0.0.0', port=5000)
